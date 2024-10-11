5e66cfc49ddacc6e957b530e9bdc0e6dec50e170
OthmanAlkubaisi <c3alot00@students.oamk.fi>
2024-10-12 00:24:27 +0300

    Home.jsx -Home.jsx koodin teko

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the application!</h1>
+            <p>This is the introductory page where you can navigate to other sections.</p>
+            <a href="/opendata">Go to the OpenData page</a>
+        </div>
+    );
+};
+
+export default Home;
