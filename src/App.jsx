1dbf0187ffb8ed6800330474b1d018930c652a16
OthmanAlkubaisi <c3alot00@students.oamk.fi>
2024-10-11 23:38:49 +0300

    app.jsx -App jsx muutos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import ComponentUI from './ComponentUI';
 import OrderForm from './OrderForm';
+import OpenData from './OpenData';
 
 function App() {
     return (
         <>
             <ComponentUI image="/sunset.jpg" title="Welcome to  product page!" />
             <OrderForm />
+            <OpenData />
         </>
     );
 }
