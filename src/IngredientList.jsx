e340743e1be0dfcb1b827435f78240c071eded24
OthmanAlkubaisi <c3alot00@students.oamk.fi>
2024-10-11 23:44:13 +0300

    IngredientList -Ingredientlist kansion luonti ja alustava koodi

---
+++ src/IngredientList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const IngredientList = ({ data }) => {
+    return (
+        <div>
+            <h3>Ingredients:</h3>
+            <ul>
+                {Object.keys(data)
+                    .filter(key => key.startsWith('strIngredient') && data[key]) // Suodatetaan vain ainesosat
+                    .map((key, index) => (
+                        <li key={index}>{data[key]}</li>
+                    ))}
+            </ul>
+        </div>
+    );
+};
+
+export default IngredientList;
